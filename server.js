06609485718c21f7619a705de3e2163ec1443488
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-07-02 11:43:08 +0530

    changes in server-side for deployment

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/api/items', items);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('../build'));
+    app.use(express.static(path.join(__dirname, "frontend", "build")))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
