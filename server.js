09acc318ba3af0bf5c073ddc364c55f76a404dcb
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-07-02 13:01:29 +0530

    minor changes

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/api/items', items);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
