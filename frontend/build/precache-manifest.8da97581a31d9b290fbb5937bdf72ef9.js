203abfb4efb4e11ec8bfebdd0332b12679f91a77
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-07-02 12:32:51 +0530

    build in FE

---
+++ frontend/build/precache-manifest.8da97581a31d9b290fbb5937bdf72ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23d515f3065e50cf6248ab2325e412c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b92012e6d7b74f570b",
+    "url": "/static/css/main.78c763d8.chunk.css"
+  },
+  {
+    "revision": "f1a7542615537075ef9a",
+    "url": "/static/js/2.b4a4d08f.chunk.js"
+  },
+  {
+    "revision": "f6b92012e6d7b74f570b",
+    "url": "/static/js/main.eaf18a5a.chunk.js"
+  },
+  {
+    "revision": "d58a5c16912709991c76",
+    "url": "/static/js/runtime-main.d431f256.js"
+  },
+  {
+    "revision": "0da10b2297f4f60819cfb2437991cd09",
+    "url": "/static/media/clouds.0da10b22.jpg"
+  }
+]);
