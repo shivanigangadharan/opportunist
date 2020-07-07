27f4273da64c6ab65c7421cf238f83598fbd8f36
shivanigangadharan <shivani.gangadharan@gmail.com>
2020-07-07 18:10:19 +0530

    1st draft of homepage, added undraw pics

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -3,83 +3,77 @@ import React, { useState } from 'react';
 import Search from '../components/search';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import clouds from '../assets/background/clouds.jpg';
 
 const Main = styled.div`
-    background-image: url(${clouds});
-    height: 101vh;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    filter: blur(0.5);
-
+    height: 100vh;
 `
 
 const Start = styled.div`
     color: black;
     margin-top: 5vh;
     padding-top: 70px;
-    padding-left: 5vw;
     padding-right: 5vw;
+    @media screen and (max-width: 1400px){
+        padding-right: 5%;
+        padding-left: 5vw;
+    }
     @media screen and (max-width: 700px){
         padding-left: 5%;
         padding-right: 5%;
     }
 `
 const Go = styled.button`
-    background: white;
     color: black;
-    font-weight: bold;
     font-size: 120%;
-    padding: 1%;
+    padding: 2%;
     border: 2px solid white;
+    border-radius: 50px;
     outline: none;
+    margin-top: 15%;
+    box-shadow: 1px 1px 3px 1px lightgrey;
     :hover{
-        background: black;
-        color: white;
+        box-shadow: 3px 3px 5px 1px darkgrey;
         transition-duration: 0.5s;
-        border: 2px solid black;
-
     }
 `
-const SearchDiv = styled.div`
-    transition-duration: 0.5s;
+const H4 = styled.h4`
+    line-height: 30px;
+    margin-top: 6%;
+`
+const Img = styled.img`
+    width: 50vw;
+    @media (max-width: 761px){
+        display: none;
+    }
 `
 function Homepage() {
-    // const [showSearch, setShowSearch] = useState(false);
-    // const [boxText, setBoxText] = useState('TRY IT');
-    // function Display() {
-    //     setShowSearch(!showSearch);
-    //     if (boxText == "TRY IT") {
-    //         setBoxText("CLOSE");
-    //     }
-    //     else {
-    //         setBoxText("TRY IT");
-    //     }
-    // }
+
     return (
         <Main>
             <Start>
-                <h1> Missing out on amazing opportunities? Look no further. </h1>
-                <h3> Find high quality and well curated set of all the opportunities awaiting you using this search tool. </h3>
-                <br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <h2> Missing out on amazing opportunities? Look no further. </h2>
+                            <H4> Find upcoming top-notch opportunities curated according to your eligibility, using this search tool. </H4>
+                            <H4> The opportunities are currently meant for students/graduates in any computer science related field. They include scholarships, open source programs, mentorship programs, internships and community events. </H4>
 
-                {/* <Go onClick={Display}
-                    aria-controls="example-collapse-text"
-                    aria-expanded={showSearch}> {boxText} </Go> */}
+                            <center>
+                                <Link to="search-results">
+                                    <Go> Know more </Go>
+                                </Link> &emsp; &emsp;
+                                <Link to="search-results">
+                                    <Go style={{ 'background': 'rgb(16, 134, 173)', 'color': 'white' }}> Get started </Go>
+                                </Link>
+                            </center>
+                        </div>
+                        <div className="col-lg-6">
+                            <Img src={require('../assets/images/welcome.png')} />
+                        </div>
+                    </div>
+                </div>
 
             </Start>
-            {/* <Collapse in={showSearch}>
-                <div id="example-collapse-text">
-                    <br /> */}
-            {/* <Search /> */}
-            <center>
-                <Link to="search-results">
-                    <Go> GET STARTED </Go>
-                </Link>
-            </center>
-            {/* </div>
-            </Collapse> */}
         </Main >
     )
 }
